a394ec734f9f0afb8293be5e073e6c6705c6f978
AnofrikovaAnna <annasa164@gmail.com>
2024-05-22 17:14:56 +0400

    Добавлена задача 2_E

---
+++ 2_E.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <cmath>
+#include <string>
+#include <set>
+
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n), b(n), res(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        a[i]--;
+        res[i] = ++b[a[i]];
+    }
+    for (int i = 1; i < n; i++) {
+        if (b[i] > b[i - 1]) {
+            cout << -1;
+            return;
+        }
+    }
+    int kl = *max_element(res.begin(), res.end());
+    cout << kl << '\n';
+    for (auto x : res) cout << x << ' ';
+}
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+}
