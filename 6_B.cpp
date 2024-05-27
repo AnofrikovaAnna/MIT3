048c102ef17c1dbcf2f4589e3783b1bf2212467c
AnofrikovaAnna <annasa164@gmail.com>
2024-05-27 17:27:36 +0400

    Добавлена задача 6_B

---
+++ 6_B.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<int> used;
+vector<vector<int>> g;
+
+void dfs(int v) {
+	for (int u : g[v]) {
+		if (!used[u]) {
+			used[u] = 1;
+			dfs(u);
+		}
+	}
+}
+
+int main(){
+	int n, m; cin >> n >> m;
+	g.assign(n, {});
+	used.assign(n, 0);
+	int a, b;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		g[a - 1].push_back(b - 1);
+		g[b - 1].push_back(a - 1);
+	}
+	used[0] = 1;
+	dfs(0);
+	if (n - 1 == m && count(used.begin(), used.end(), 1) == n)
+		cout << "YES";
+	else
+		cout << "NO";
+}
