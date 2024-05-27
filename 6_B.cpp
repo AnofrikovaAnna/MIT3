82f7bc153ab9e60086a766881f46c9d5361a5638
AnofrikovaAnna <annasa164@gmail.com>
2024-05-27 17:50:43 +0400

    Изменена задача 6_B: добавлена предпроверка соотношения n и m

--- 6_B.cpp
+++ 6_B.cpp
@@ -20,16 +20,20 @@ int main(){
 	int n, m; cin >> n >> m;
 	g.assign(n, {});
 	used.assign(n, 0);
-	int a, b;
-	for (int i = 0; i < m; i++) {
-		cin >> a >> b;
-		g[a - 1].push_back(b - 1);
-		g[b - 1].push_back(a - 1);
-	}
-	used[0] = 1;
-	dfs(0);
-	if (n - 1 == m && count(used.begin(), used.end(), 1) == n)
-		cout << "YES";
-	else
+	if (n - 1 != m)
 		cout << "NO";
+	else {
+		int a, b;
+		for (int i = 0; i < m; i++) {
+			cin >> a >> b;
+			g[a - 1].push_back(b - 1);
+			g[b - 1].push_back(a - 1);
+		}
+		used[0] = 1;
+		dfs(0);
+		if (count(used.begin(), used.end(), 1) == n)
+			cout << "YES";
+		else
+			cout << "NO";
+	}
 }
